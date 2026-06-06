fd2bacf0e568c146d73175bc823ea079d7e8bd30
John Peter Sa <156824256+johnpetersa19@users.noreply.github.com>
2026-06-06 14:14:57 -0300

    fix: upgrade rust-stable to 25.08 and add missing GTK interfaces in glib::wrapper!

--- src/window.rs
+++ src/window.rs
@@ -101,7 +101,15 @@ mod imp {
 glib::wrapper! {
     pub struct TemporalExplorerWindow(ObjectSubclass<imp::TemporalExplorerWindow>)
         @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow, adw::ApplicationWindow,
-        @implements gio::ActionGroup, gio::ActionMap;
+        @implements
+            gio::ActionGroup,
+            gio::ActionMap,
+            gtk::Accessible,
+            gtk::Buildable,
+            gtk::ConstraintTarget,
+            gtk::Native,
+            gtk::Root,
+            gtk::ShortcutManager;
 }
 
 impl TemporalExplorerWindow {
